bde7a613749bc52f29e422b6a0a672ef32d0f99c
DELL <sakshi05082000@gmail.com>
2022-03-10 15:55:47 +0530

    final commit

---
+++ app/src/main/java/com/sakshi/majorproject/Skitsplashscreen.kt
@@ -0,0 +1,14 @@
+package com.sakshi.majorproject
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class Skitsplashscreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_skitsplashscreen)
+
+    }
+}
